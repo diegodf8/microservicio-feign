4a67a93a9bb25f3a6659588aedb1695dc28f8b79
diegodf8 <diegodf8@gmail.com>
2018-07-15 18:20:57 +0200

    Usuarios API rest CRUD OK

--- src/main/java/com/cice/microserviciofeign/feign/Productos.java
+++ src/main/java/com/cice/microserviciofeign/feign/Productos.java
@@ -2,9 +2,13 @@ package com.cice.microserviciofeign.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
 
 @FeignClient("microproducto")
 public interface Productos {
-    @RequestMapping("/get-productos")
-    Long getProductos(String idUsuario);
+    @RequestMapping(name="/get-productos",method=RequestMethod.GET)
+    List<Long> getProductos(@PathParam(value = "idUsuario") String idUsuario);
 }
